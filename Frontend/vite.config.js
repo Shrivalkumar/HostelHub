88439c21deb40d36687c960135604b857a778716
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2025-05-25 03:37:16 +0530

    fix: update frontend dependencies and configurations for deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true
+  }
 })
